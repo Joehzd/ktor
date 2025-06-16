47ffac4e2a2c6bea2ff09134bc03885c28945b28
houzhendong <houzhendong@moonshot.cn>
2025-06-16 11:47:58 +0800

    publish url

--- build-logic/src/main/kotlin/ktorbuild/internal/publish/Repositories.kt
+++ build-logic/src/main/kotlin/ktorbuild/internal/publish/Repositories.kt
@@ -7,17 +7,20 @@ package ktorbuild.internal.publish
 import org.gradle.api.artifacts.dsl.RepositoryHandler
 
 internal fun RepositoryHandler.addTargetRepositoryIfConfigured() {
-    val repositoryId = System.getenv("REPOSITORY_ID").orEmpty()
-    val publishingUrl = if (repositoryId.isNotBlank()) {
-        println("Set publishing to repository $repositoryId")
-        "https://oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId"
-    } else {
-        System.getenv("PUBLISHING_URL")
-    }
-    if (publishingUrl == null) return
+//    val repositoryId = System.getenv("REPOSITORY_ID").orEmpty()
+//    val publishingUrl = if (repositoryId.isNotBlank()) {
+//        println("Set publishing to repository $repositoryId")
+//        "https://oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId"
+//    } else {
+//        System.getenv("PUBLISHING_URL")
+//    }
+//    if (publishingUrl == null) return
 
+    // 不能开 feilian
+    val publishingUrl = "http://192.168.87.17:8081/repository/kimi-android/"
     maven {
         setUrl(publishingUrl)
+        isAllowInsecureProtocol = true
         credentials {
             username = System.getenv("PUBLISHING_USER")
             password = System.getenv("PUBLISHING_PASSWORD")
